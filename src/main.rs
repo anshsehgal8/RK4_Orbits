7eba96f715692baf99181a7e9189013658bc139a
Ansh Sehgal <anshsehgal8@gmail.com>
2020-11-20 13:19:02 -0800

    Create 8 component state vectors

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,44 @@
 use std::ops::Mul;
+use std::ops::Add;
+//use std::num::Float;
 
 static GRAVITATIONAL_CONSTANT_G: f64 = 6.67E-8;
-static MASS_RATIO:f64 = 0.01;
+static MASS_RATIO:f64 = 0.5;
 
 // ============================================================================
 
 struct TwoBodyState
 {
-    x1: f64,
-    y1: f64,
+    x1:  f64,
+    y1:  f64,
     vx1: f64,
     vy1: f64,
-    x2: f64,
-    y2: f64,
+    x2:  f64,
+    y2:  f64,
     vx2: f64,
     vy2: f64,
 }
 
 
-// pub trait AddState {
-//     fn add_pos_x(&self) -> f64;
-//     fn add_pos_y(&self) -> f64;
-//     fn add_vel_x(&self) -> f64;
-//     fn add_vel_y(&self) -> f64;
-// }
 
-pub trait Multipy{
-    fn mul(&self, scale: f64) -> f64;
+struct DerivativeState{
+    vx1: f64,
+    vy1: f64,
+    ax1: f64,
+    ay1: f64,
+    vx2: f64,
+    vy2: f64,
+    ax2: f64,
+    ay2: f64,
 }
 
 
-// impl AddState for TwoBodyState{
-//     fn add_pos_x(&self) -> f64 {
-//         return self.x1 + self.x2 ;
-//     }
-//     fn add_pos_y(&self) -> f64 {
-//         return self.y1 + self.y2 ;
-//     }
-//     fn add_vel_x(&self) -> f64 {
-//         return self.vx1 + self.vx2 ; 
-//     }
-//     fn add_vel_y(&self) -> f64 {
-//         return self.vy1 + self.vy2 ; 
-//     }
-// }
 
 
-impl std::ops::Mul<f64> for TwoBodyState
+impl Mul<f64> for TwoBodyState
 {
     type Output = TwoBodyState;
-    fn mul(&self, scale: f64) -> TwoBodyState
+    fn mul(self, scale: f64) -> TwoBodyState
     {
         TwoBodyState
         {
@@ -68,100 +57,100 @@ impl std::ops::Mul<f64> for TwoBodyState
 
 
 
-// fn main()
-// {
-//     let mut s1 = TwoBodyState
-//     {
-//         x1: 0.,
-//         y1: 0.,
-//         vx1: 0.,
-//         vy1: 0.,
-//         x2: 1.,
-//         y2: 0.,
-//         vx2: 0.2,
-//         vy2: 0.3,
-
-//     };
-
-//     let mut s2 = TwoBodyState 
-//     {
-//         x1: 0.2,
-//         y1: 0.3,
-//         vx1: 0.1,
-//         vy1: 0.1,
-//         x2: 1.2,
-//         y2: 0.2,
-//         vx2: 0.1,
-//         vy2: 0.4,
-//     };
-
-//     s1.mul(5.0);
-//     println!("{}", s1.x1 );
-
-// }
-
-
-
-
-fn derivative_pos_x(ux: [f64;2]) -> f64
+impl Mul<f64> for DerivativeState
 {
-    let vx = ux[1];
-    vx
+    type Output = DerivativeState;
+    fn mul(self, scale: f64) -> DerivativeState
+    {
+        DerivativeState
+        {
+            vx1: self.vx1 * scale,
+            vy1: self.vy1 * scale,
+            ax1: self.ax1 * scale,
+            ay1: self.ay1 * scale,
+            vx2: self.vx2 * scale,
+            vy2: self.vy2 * scale,
+            ax2: self.ax1 * scale,
+            ay2: self.ay2 * scale,
+        }
+    }
 }
 
 
 
 
-fn derivative_pos_y(uy: [f64;2]) -> f64
+
+impl Add for TwoBodyState
 {
-    let vy = uy[1];
-    vy
+    type Output = TwoBodyState;
+    fn add(self, other: TwoBodyState) -> TwoBodyState
+    {
+        TwoBodyState
+        {
+            x1: self.x1 + other.x1,
+            y1: self.y1 + other.x2,
+            vx1: self.vx1 + other.vx1,
+            vy1: self.vy1 + other.vy2,
+            x2: self.x2 + other.x2,
+            y2: self.y2 + other.y2,
+            vx2: self.vx2 + other.vx2,
+            vy2: self.vy2 + other.vy2,
+        }
+    }
+}
+
+
+
+fn derivative(state_vector: TwoBodyState) -> DerivativeState
+{
+
+    let x1 = state_vector.x1;
+    let y1 = state_vector.y1;
+    let vx1 = state_vector.vx1;
+    let vy1 = state_vector.vy1;
+    let x2 = state_vector.x2;
+    let y2 = state_vector.y2;
+    let vx2 = state_vector.vx2;
+    let vy2 = state_vector.vy2;
+    
+    let r_squared_1 = x1.powi(2) + y1.powi(2) ;
+    let r_squared_2 = x2.powi(2) + y2.powi(2) ;
+
+    let rhat_x1  = x1 / (x1.powi(2) + y1.powi(2)).sqrt();
+    let rhat_y1  = y1 / (x1.powi(2) + y1.powi(2)).sqrt();
+    let rhat_x2  = x2 / (x2.powi(2) + y2.powi(2)).sqrt();
+    let rhat_y2  = y2 / (x2.powi(2) + y2.powi(2)).sqrt();
+
+    let ax1 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_1 * MASS_RATIO)) * rhat_x1;
+    let ay1 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_1 * MASS_RATIO)) * rhat_y1;
+    let ax2 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_2 * MASS_RATIO)) * rhat_x2;
+    let ay2 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_2 * MASS_RATIO)) * rhat_y2;
+
+    DerivativeState{
+        vx1: vx1,
+        vy1: vx2,
+        ax1: ax1,
+        ay1: ay1,
+        vx2: vx2,
+        vy2: vy2,
+        ax2: ax2,
+        ay2: ay2,
+    }
 }
 
 
 
 
-fn derivative_vel_x(ux: [f64;2]) -> f64
+
+fn rk4(state_vector: TwoBodyState, dt: f64) -> [f64; 8]
 {
-    let ax = - (GRAVITATIONAL_CONSTANT_G * (1. + MASS_RATIO)) / (ux[0].powi(2));
-    ax
-}
+    let k1 = dt * derivative(state_vector);
+    let k2 = dt * derivative(state_vector + k1 * 0.5);
+    let k3 = dt * derivative(state_vector + k2 * 0.5);
+    let k4 = dt * derivative(state_vector + k3 * 1.0);
 
 
-
-
-fn derivative_vel_y(uy: [f64;2]) -> f64
-{
-    let ay = - (GRAVITATIONAL_CONSTANT_G * (1. + MASS_RATIO)) / (uy[0].powi(2));
-    ay
-}
-
-
-
-
-fn rk4(ux: [f64; 2], uy: [f64;2], dt: f64) -> [f64; 4]
-{
-    let k1x = dt * derivative_pos_x(ux);
-    let k1y = dt * derivative_pos_y(uy);
-    let l1x = dt * derivative_vel_x(ux);
-    let l1y = dt * derivative_vel_y(uy);
-    let k2x = dt * derivative_pos_x([ux[0] + k1x * 0.5, ux[1] + l1x * 0.5]);
-    let k2y = dt * derivative_pos_y([uy[0] + k1y * 0.5, uy[1] + l1y * 0.5]);
-    let l2x = dt * derivative_vel_x([ux[0] + k1x * 0.5, ux[1] + l1x * 0.5]);
-    let l2y = dt * derivative_vel_y([uy[0] + k1y * 0.5, uy[1] + l1y * 0.5]);
-    let k3x = dt * derivative_pos_x([ux[0] + k2x * 0.5, ux[1] + l2x * 0.5]);
-    let k3y = dt * derivative_pos_y([uy[0] + k2y * 0.5, uy[1] + l2y * 0.5]);
-    let l3x = dt * derivative_vel_x([ux[0] + k2x * 0.5, ux[1] + l2x * 0.5]);
-    let l3y = dt * derivative_vel_y([uy[0] + k2y * 0.5, uy[1] + l2y * 0.5]);
-    let k4x = dt * derivative_pos_x([ux[0] + k3x * 1.0, ux[1] + l3x * 1.0]);
-    let k4y = dt * derivative_pos_y([uy[0] + k3y * 1.0, uy[1] + l3y * 1.0]);
-    let l4x = dt * derivative_vel_x([ux[0] + k3x * 1.0, ux[1] + l3x * 1.0]);
-    let l4y = dt * derivative_vel_y([uy[0] + k3y * 1.0, uy[1] + l3y * 1.0]);
-    let x_new = ux[0] + (k1x + 2.0 * k2x + 2.0 * k3x + k4x) / 6.0;
-    let y_new = uy[0] + (k1y + 2.0 * k2y + 2.0 * k3y + k4y) / 6.0;
-    let vx_new = ux[1] + (l1x + 2.0 * l2x + 2.0 * l3x + l4x) / 6.0;
-    let vy_new = uy[1] + (l1y + 2.0 * l2y + 2.0 * l3y + l4y) / 6.0;
-    let u = [x_new, y_new, vx_new, vy_new];
+    let u = state_vector + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
     u
 }
 
@@ -171,23 +160,21 @@ fn rk4(ux: [f64; 2], uy: [f64;2], dt: f64) -> [f64; 4]
 fn main()
 {
     let s1 = TwoBodyState{
-        x: 0.0,
-        y: 0.0,
-        vx: 0.0,
-        vy: 0.0,
+        x1: 0.2,
+        y1: 0.3,
+        vx1: 0.1,
+        vy1: 0.1,
+        x2: 1.0,
+        y2: 0.0,
+        vx2: 0.2,
+        vy2: 0.3,
     };
 
-    let s2 = TwoBodyState{
-        x: 1.0,
-        y: 0.0,
-        vx: 0.2,
-        vy: 0.3,
-    };
-    let mut u = [1.0,0.0,0.5,0.1]; // [x, y, xdot, ydot]
+
     let mut t = 0.0;
     let dt = 0.01;
     while t < 2.0 {
-        u = rk4([u[0],u[2]], [u[1],u[3]], dt);
+        let mut u = rk4(s1,dt);
         println!("{:+0.8} {:+0.8} {:+0.8}", t, u[0], u[1]);
         t = t + dt;
     }
