1603803d47b3569909196849c2631a21101510c0
Ansh Sehgal <anshsehgal8@gmail.com>
2020-11-23 16:27:36 -0800

    Numerical integration implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::ops::Mul;
 use std::ops::Add;
-//use std::num::Float;
+//use hdf5::File;
 
-static GRAVITATIONAL_CONSTANT_G: f64 = 6.67E-8;
-static MASS_RATIO:f64 = 0.5;
+static GRAVITATIONAL_CONSTANT_G: f64 = 1.0; //6.67E-2;
+static MASS1:f64 = 1.0;
+static MASS2:f64 = 1.0;
 
 // ============================================================================
+#[derive(Copy, Clone)]
 
 struct TwoBodyState
 {
@@ -20,7 +22,7 @@ struct TwoBodyState
 }
 
 
-
+#[derive(Copy, Clone)]
 struct DerivativeState{
     vx1: f64,
     vy1: f64,
@@ -33,6 +35,22 @@ struct DerivativeState{
 }
 
 
+impl DerivativeState{
+    fn to_twobodystate(&self) -> TwoBodyState {
+        TwoBodyState{
+            x1:  self.vx1,
+            y1:  self.vy1,
+            vx1: self.ax1,
+            vy1: self.ay1,
+            x2:  self.vx2,
+            y2:  self.vy2,
+            vx2: self.ax2,
+            vy2: self.ay2,
+            
+        }
+    }
+}
+
 
 
 impl Mul<f64> for TwoBodyState
@@ -42,18 +60,17 @@ impl Mul<f64> for TwoBodyState
     {
         TwoBodyState
         {
-            x1: self.x1 * scale,
-            x2: self.x2 * scale,
+            x1:  self.x1  * scale,
+            x2:  self.x2  * scale,
             vx1: self.vx1 * scale,
             vx2: self.vx2 * scale,
-            y1: self.y1 * scale,
-            y2: self.y2 * scale,
+            y1:  self.y1  * scale,
+            y2:  self.y2  * scale,
             vy1: self.vy1 * scale,
             vy2: self.vy2 * scale,
         }
     }
 }
-
 
 
 
@@ -70,13 +87,11 @@ impl Mul<f64> for DerivativeState
             ay1: self.ay1 * scale,
             vx2: self.vx2 * scale,
             vy2: self.vy2 * scale,
-            ax2: self.ax1 * scale,
+            ax2: self.ax2 * scale,
             ay2: self.ay2 * scale,
         }
     }
 }
-
-
 
 
 
@@ -87,12 +102,12 @@ impl Add for TwoBodyState
     {
         TwoBodyState
         {
-            x1: self.x1 + other.x1,
-            y1: self.y1 + other.x2,
+            x1:  self.x1  + other.x1,
+            y1:  self.y1  + other.y1,
             vx1: self.vx1 + other.vx1,
-            vy1: self.vy1 + other.vy2,
-            x2: self.x2 + other.x2,
-            y2: self.y2 + other.y2,
+            vy1: self.vy1 + other.vy1,
+            x2:  self.x2  + other.x2,
+            y2:  self.y2  + other.y2,
             vx2: self.vx2 + other.vx2,
             vy2: self.vy2 + other.vy2,
         }
@@ -101,34 +116,24 @@ impl Add for TwoBodyState
 
 
 
-fn derivative(state_vector: TwoBodyState) -> DerivativeState
-{
-
-    let x1 = state_vector.x1;
-    let y1 = state_vector.y1;
+fn derivative(state_vector: &TwoBodyState) -> DerivativeState{
+    let x1  = state_vector.x1;
+    let y1  = state_vector.y1;
     let vx1 = state_vector.vx1;
     let vy1 = state_vector.vy1;
-    let x2 = state_vector.x2;
-    let y2 = state_vector.y2;
+    let x2  = state_vector.x2;
+    let y2  = state_vector.y2;
     let vx2 = state_vector.vx2;
-    let vy2 = state_vector.vy2;
-    
-    let r_squared_1 = x1.powi(2) + y1.powi(2) ;
-    let r_squared_2 = x2.powi(2) + y2.powi(2) ;
-
-    let rhat_x1  = x1 / (x1.powi(2) + y1.powi(2)).sqrt();
-    let rhat_y1  = y1 / (x1.powi(2) + y1.powi(2)).sqrt();
-    let rhat_x2  = x2 / (x2.powi(2) + y2.powi(2)).sqrt();
-    let rhat_y2  = y2 / (x2.powi(2) + y2.powi(2)).sqrt();
-
-    let ax1 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_1 * MASS_RATIO)) * rhat_x1;
-    let ay1 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_1 * MASS_RATIO)) * rhat_y1;
-    let ax2 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_2 * MASS_RATIO)) * rhat_x2;
-    let ay2 = -(GRAVITATIONAL_CONSTANT_G / (r_squared_2 * MASS_RATIO)) * rhat_y2;
+    let vy2 = state_vector.vy2;  
+    let r   = ((x1-x2).powf(2.0) + (y1-y2).powf(2.0)).powf(0.5);
+    let ax1 = -(GRAVITATIONAL_CONSTANT_G * MASS2 / (r.powi(3) )) * (x1-x2);
+    let ay1 = -(GRAVITATIONAL_CONSTANT_G * MASS2 / (r.powi(3) )) * (y1-y2);
+    let ax2 = -(GRAVITATIONAL_CONSTANT_G * MASS1 / (r.powi(3) )) * (x2-x1);
+    let ay2 = -(GRAVITATIONAL_CONSTANT_G * MASS1 / (r.powi(3) )) * (y2-y1);
 
     DerivativeState{
         vx1: vx1,
-        vy1: vx2,
+        vy1: vy1,
         ax1: ax1,
         ay1: ay1,
         vx2: vx2,
@@ -141,41 +146,39 @@ fn derivative(state_vector: TwoBodyState) -> DerivativeState
 
 
 
-
-fn rk4(state_vector: TwoBodyState, dt: f64) -> [f64; 8]
+fn rk4(state_vector: TwoBodyState, dt: f64) -> TwoBodyState
 {
-    let k1 = dt * derivative(state_vector);
-    let k2 = dt * derivative(state_vector + k1 * 0.5);
-    let k3 = dt * derivative(state_vector + k2 * 0.5);
-    let k4 = dt * derivative(state_vector + k3 * 1.0);
-
-
-    let u = state_vector + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
+    let k1 = derivative(&state_vector).to_twobodystate() * dt;
+    let a2 = state_vector + k1 * 0.5;
+    let k2 = derivative(&a2).to_twobodystate() * dt;
+    let a3 = state_vector + k2 * 0.5;
+    let k3 = derivative(&a3).to_twobodystate() * dt;
+    let a4 = state_vector + k3 * 1.0;
+    let k4 = derivative(&a4).to_twobodystate() * dt;
+    let u = state_vector + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (1.0/6.0);
     u
 }
 
 
-
-
 fn main()
 {
-    let s1 = TwoBodyState{
-        x1: 0.2,
-        y1: 0.3,
-        vx1: 0.1,
-        vy1: 0.1,
-        x2: 1.0,
-        y2: 0.0,
-        vx2: 0.2,
-        vy2: 0.3,
+    let mut s1 = TwoBodyState{
+        x1:  -0.5,
+        y1:   0.0,
+        vx1:  0.0,
+        vy1: -0.5,
+        x2:   0.5,
+        y2:   0.0,
+        vx2:  0.0,
+        vy2:  0.5,
     };
-
-
     let mut t = 0.0;
     let dt = 0.01;
-    while t < 2.0 {
-        let mut u = rk4(s1,dt);
-        println!("{:+0.8} {:+0.8} {:+0.8}", t, u[0], u[1]);
+    while t < 20.0 {
+        s1 = rk4(s1,dt);
+        println!("{:+0.8} {:+0.8} {:+0.8} {:+0.8} {:+0.8}", t, s1.x1, s1.y1, s1.x2, s1.y2);
         t = t + dt;
     }
+
+    //let file = File::open("output.h5");
 }
